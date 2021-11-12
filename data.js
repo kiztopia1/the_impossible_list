c17e9b38e00868e2f0e06a2cafa97ac12624083f
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-11-12 03:23:30 -0700

    make list displaying skeleton and add some data

---
+++ data.js
@@ -0,0 +1,41 @@
+var data = {
+    catagories: [
+        {
+            name:'work',
+            data:[
+                {
+                    name:'build my app yene boss',
+                    status: false,
+                },
+                {
+                    name:'learn vue and bild amazing apps with it',
+                    status: false,
+                },
+            ]
+        }
+    
+        ,
+        {
+            name:'self development ',
+            data : [
+                {
+                    name:'read 12 books ',
+                    status: false,
+                },
+                {
+                    name:'learn how to kill peoples',
+                    status: false,
+                },
+            ]
+        }
+            
+
+        
+    ],
+    users: [
+        {
+            name: 'kirubel demelash ',
+            password: 'killing 354'
+        }
+    ]
+}
