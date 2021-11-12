538a196ea51c667460f49f1caeb743c317994547
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-11-12 04:08:30 -0700

    status changer added for lists

--- data.js
+++ data.js
@@ -6,10 +6,12 @@ var data = {
                 {
                     name:'build my app yene boss',
                     status: false,
+                    id:1
                 },
                 {
                     name:'learn vue and bild amazing apps with it',
                     status: false,
+                    id:2
                 },
             ]
         }
@@ -21,10 +23,12 @@ var data = {
                 {
                     name:'read 12 books ',
                     status: false,
+                    id:3
                 },
                 {
                     name:'learn how to kill peoples',
                     status: false,
+                    id:4
                 },
             ]
         }
