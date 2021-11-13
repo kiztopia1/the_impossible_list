b1d0469bf6c894cee905eff800fbb8187944cfc4
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-11-12 22:49:15 -0700

    adding style on tasks

--- data.js
+++ data.js
@@ -1,45 +1,22 @@
-var data = {
-    catagories: [
+var db = [
         {
-            name:'work',
-            data:[
+            id:'1',
+            userName:'kirubel demelash',
+            tasks:[
                 {
+                    id:1,
                     name:'build my app yene boss',
                     status: false,
-                    id:1
+                    catagories: 'work',
+                    sub_tasks:[]
                 },
                 {
+                    id:2,
                     name:'learn vue and bild amazing apps with it',
                     status: false,
-                    id:2
+                    catagories: 'work',
+                    sub_tasks:[]
                 },
             ]
-        }
-    
-        ,
-        {
-            name:'self development ',
-            data : [
-                {
-                    name:'read 12 books ',
-                    status: false,
-                    id:3
-                },
-                {
-                    name:'learn how to kill peoples',
-                    status: false,
-                    id:4
-                },
-            ]
-        }
-            
-
-        
-    ],
-    users: [
-        {
-            name: 'kirubel demelash ',
-            password: 'killing 354'
         }
     ]
-}
